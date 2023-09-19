81988a64953a3cec6f58ca7af2a158b24755c5bf
batoolalali <balali@avertra.com>
2023-09-19 09:09:34 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,22 @@
+console.log("hi")
+
+let ulEl = document.getElementById("device");
+
+console.log(WURFL)
+
+for (const key in WURFL) {
+
+
+    let liEl = document.createElement("li");
+    ulEl.appendChild(liEl)
+    liEl.innerHTML = `${key}: ${WURFL[key]}`
+}
+
+
+
+const userAgent = navigator.userAgent.toLowerCase();
+const isTablet = /(ipad|tablet|(android(?!.*mobile))|(windows(?!.*phone)(.*touch))|kindle|playbook|silk|(puffin(?!.*(IP|AP|WP))))/.test(userAgent);
+
+
+let h1El = document.getElementById("isTablet");
+h1El.innerHTML = `is Tablet: ${isTablet}`
