9eaaf9d7271e94f08719b80517382126eb7971fd
batoolalali <balali@avertra.com>
2023-09-20 08:22:16 +0000

    agent

--- app.js
+++ app.js
@@ -18,5 +18,10 @@ const userAgent = navigator.userAgent.toLowerCase();
 const isTablet = /(ipad|tablet|(android(?!.*mobile))|(windows(?!.*phone)(.*touch))|kindle|playbook|silk|(puffin(?!.*(IP|AP|WP))))/.test(userAgent);
 
 
+
 let h1El = document.getElementById("isTablet");
 h1El.innerHTML = `is Tablet: ${isTablet}`
+
+
+let pEl = document.getElementById("agent");
+pEl.innerHTML= `user agent: ${navigator.userAgent}`
